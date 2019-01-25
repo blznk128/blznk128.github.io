d73fd826ebe152c63d838ae9c493a943233f8ade
Benny Borunda <blznk128@yahoo.com>
2019-01-25 16:44:18 -0700

    hey

---
+++ js/typedInfo.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".display-4", {
+    strings: [
+        " honesty", " integrity", " hard work", " passion"
+    ],
+    typeSpeed: 150,
+    backSpeed: 50,
+    loop: true
+  
+  });
